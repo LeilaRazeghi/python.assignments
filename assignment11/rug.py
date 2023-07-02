d45d44353b48a763ca6a5368ec5b190b8b84ecaf
LeilaRz <leila.ry@gmail.com>
2023-07-02 00:41:58 -0700

    tamrin

--- assignment11/rug.py
+++ assignment11/rug.py
@@ -1,17 +1,23 @@
+n = int(input("Enter a number: "))
 
-n = int(input("enter your rug size(n*n): "))
-
-pattern = ["🟥","🟧","🟨","🟩","🟦","🟪","🟫"]
 def generate_rug(n):
-    f = 0
-    for row in range(n):
-        for i in range(1, n + 1):
-            print(pattern[f], end="")
-            f += 1
-            if f == 6:
-                f = 0
-        print()
+    if n % 2 == 0:
+        return "Please enter an odd number."
     
+    emojis = ["⚪", "🟡", "🟠", "🔴", "🟢", "🔵", "🟣", "🟤", "⚫"]
+    rug = [[0] * n for _ in range(n)]
+    middle = n // 2
+    
+    for i in range(middle, -1, -1):
+        for j in range(n):
+            rug[i][j] = emojis[middle - i]
+            rug[n-1-i][j] = emojis[middle - i]
+            rug[j][i] = emojis[middle - i]
+            rug[j][n-1-i] = emojis[middle - i]
+            
+    for row in rug:
+        for c in row:
+            print(c, end=' ')
+        print()
 
 generate_rug(n)
-        
